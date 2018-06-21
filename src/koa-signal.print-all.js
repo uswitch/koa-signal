af50454c3989818b3fb6266942770108e971d085
Dom Charlesworth <dgc336@gmail.com>
2018-06-21 10:38:30 +0100

    💻 Improve the display of emojis and use box drawing lines

--- src/koa-signal.print-all.js
+++ src/koa-signal.print-all.js
@@ -10,6 +10,13 @@ signal.end('Stopping!')
 signal.success('Everything worked out okay')
 
 console.log()
+console.log(dim('-- Logging functions'))
+
+signal.access({ req: { path: '/post/to/a/path', method: 'POST' }, res: { responseTime: 200, statusCode: 503 } })
+signal.access({ req: { path: '/get/a/resource', method: 'GET' }, res: { responseTime: 1232, statusCode: 200 } })
+signal.access({ req: { path: '/unknown', method: 'GET' }, res: { responseTime: 1232, statusCode: 404 } })
+
+console.log()
 console.log(dim('-- Runtime functions'))
 
 signal.trace({
@@ -22,10 +29,3 @@ signal.trace({
 signal.info('This is an info message')
 signal.warn('This is a warning message')
 signal.error('Error message', new Error('This is an error'))
-
-console.log()
-console.log(dim('-- Logging functions'))
-
-signal.access({ req: { path: '/post/to/a/path', method: 'POST' }, res: { responseTime: 200, statusCode: 503 } })
-signal.access({ req: { path: '/get/a/resource', method: 'GET' }, res: { responseTime: 1232, statusCode: 200 } })
-signal.access({ req: { path: '/unknown', method: 'GET' }, res: { responseTime: 1232, statusCode: 404 } })
