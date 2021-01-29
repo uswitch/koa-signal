d59999ef92de227c10e0a30e496235aa85ae9ba1
Phil Helm <github@phelm.co.uk>
2021-01-29 11:46:10 +0000

    get rid of require-directory as it was causing problems

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'access-log': require('./access-log.js'),
+  'access': require('./access.js'),
+  'error': require('./error.js'),
+  'file-name': require('./file-name.js'),
+  'id': require('./id.js'),
+  'json': require('./json.js'),
+  'level': require('./level.js'),
+  'message': require('./message.js'),
+  'scope': require('./scope.js'),
+  'summary': require('./summary.js'),
+  'time-diff-init': require('./time-diff-init.js'),
+  'time-diff-scope': require('./time-diff-scope.js'),
+  'zipkin-level': require('./zipkin-level.js')
+}
